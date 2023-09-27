a48be47322cd265186071ab6e9857e66e87992ec
StephanieSDL <94206446+StephanieSDL@users.noreply.github.com>
2023-09-27 14:20:18 -0700

    Added ID check in addDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -15,9 +15,21 @@ import {
 export default class InsightFacade implements IInsightFacade {
 	constructor() {
 		console.log("InsightFacadeImpl::init()");
+		private datasets: Map<string, InsightDataset> = new Map();
 	}
 
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+		// Check if id is blank, includes underscore, or complete whitespace.
+		if (id === "" || /[_\s]/.test(id)) {
+			return Promise.reject(new InsightError('Invalid id'));
+		}
+
+		// Check if id already exists.
+		if (this.datasets.has(id)) {
+			return Promise.reject(new InsightError('Dataset already exists'));
+		}
+
+
 		return Promise.reject("Not implemented.");
 	}
 
