8d1946afa76532bac916fb24edc67434d601bb3d
karry <karry.lang@gmail.com>
2023-10-31 16:54:27 -0700

    refactered InsightFacade.ts

--- src/controller/utilities/addRooms.ts
+++ src/controller/utilities/addRooms.ts
@@ -13,30 +13,29 @@ export async function processRoomsDataset(content: string) {
 		if (indexFile) {
 			const indexDocument = parse5.parse(indexFile);
 
-			// recursively search the node tree for building list table
-			const validTtbody = locateTable(indexDocument);
-
-			// after finding a valid table, save to buildingList
-			if (validTtbody) {
-				const buildingList = getBuildings(validTtbody);
-				return ["edit THIS!"];	// TODO:  add rooms
+			// Recursively search the node tree for building list table
+			const validTable = locateTable(indexDocument);
+			if (validTable) {
+				// TODO:  add rooms
+				return ["edit THIS!"];
 			} else {
 				throw new Error("index.htm contains no table");
 			}
 		}
+
 		return null;
 	} catch (error) {
 		throw new InsightError("Invalid rooms content");
 	}
 }
 
-function locateTable(node: any): any | null {
+function locateTable(node: any): any[] | null {
 	// invalid if index.htm contains no table
 	if (!node) {
 		return null;
 	}
 	// If current node is a table, check if it is the building list table
-	if (node.tagName === "tbody") {
+	if (node.tagName === "table") {
 		if (isValidTable(node)) {
 			return node;
 		}
@@ -55,53 +54,29 @@ function locateTable(node: any): any | null {
 
 // check if node is building list table
 function isValidTable(node: any): boolean {
-	let trNode = null;
-	for (const tr of node.childNodes) {
-		if (tr.tagName === "tr") {
-			trNode = tr;
+	let theadNode = null;
+	for (const child of node.childNodes) {
+		if (child.tagName === "thead") {
+			theadNode = child;
 			break;
 		}
 	}
 
 	// iterate through each th in the thead to see if it is the building list table
-	for (const tdNode of trNode.childNodes) {
-		if (tdNode.tagName === "td") {
-			if (tdNode.attrs && tdNode.attrs.some((attr: {name: string, value: string}) =>
-				attr.name === "class" &&
-				attr.value === "views-field views-field-field-building-image")) {
-				// only return true when spot the building list table column
-				return true;
+	for (const trNode of theadNode.childNodes) {
+		if (trNode.tagName === "tr") {
+			for (const thNode of trNode.childNodes) {
+				if (thNode.tagName === "th") {
+					if (thNode.attrs && thNode.attrs.some((attr: {name: string, value: string}) =>
+						attr.name === "class" &&
+						attr.value === "views-field views-field-field-building-image")) {
+						// only return true when spot the building list table column
+						return true;
+					}
+				}
 			}
 		}
 	}
 
 	return false;
-}
-
-function getBuildings(tbody: any){
-	let buildings: Map<string, any[]> = new Map();
-	for (let tr of tbody.childNodes) {
-		if (tr.nodeName === "tr" && tr.childNodes) {
-			const code = getColumnData(tr, "views-field views-field-field-building-code");
-			const name = getColumnData(tr, "views-field views-field-title");
-			const address = getColumnData(tr, "views-field views-field-field-building-address");
-			const link = getColumnData(tr, "views-field views-field-nothing");
-			buildings.set(code, [name, address, link]);
-		}
-	}
-	return buildings;
-}
-
-function getColumnData(tr: any, className: string) {
-	for (let td of tr.childNodes) {
-		if (td.nodeName === "td" && td.attrs[0].name === "class" && td.attrs[0].value.includes(className)) {
-			if (className === "views-field views-field-title") {
-				return td.childNodes[1].childNodes[0].value.trim();
-			} else if (className === "views-field views-field-nothing") {
-				return td.childNodes[1].attrs[0].value.trim();
-			} else {
-				return td.childNodes[0].value.trim();
-			}
-		}
-	}
 }
