c06d909880798990d3f8d2fb2601ca18841c62c8
karry <karry.lang@gmail.com>
2023-11-23 02:27:34 -0800

    test cases

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -1,18 +1,22 @@
 import Server from "../../src/rest/Server";
 import InsightFacade from "../../src/controller/InsightFacade";
-
+import {InsightDatasetKind} from "../../src/controller/IInsightFacade";
+import {clearDisk, getContentFromArchives} from "../TestUtil";
 import {expect} from "chai";
 import request, {Response} from "supertest";
+import * as fs from "fs-extra";
 
 describe("Facade D3", function () {
-
+	this.timeout(100000);
 	let facade: InsightFacade;
 	let server: Server;
+	let sections: string;
 	const SERVER_URL = "http://localhost:4321";
 
 	before(function () {
 		facade = new InsightFacade();
 		server = new Server(4321);
+		sections = getContentFromArchives("pair.zip");
 		// TODO: start server here once and handle errors properly
 		return server.start();
 	});
@@ -33,101 +37,117 @@ describe("Facade D3", function () {
 	});
 
 	// Sample on how to format PUT requests
-	// it("PUT test for sections dataset", function () {
-	// 	const ENDPOINT_URL = "/dataset/ubc/sections";
-	// 	const ZIP_FILE_DATA = "test/resources/archives/pair.zip";
-	// 	try {
-	// 		return request(SERVER_URL)
-	// 			.put(ENDPOINT_URL)
-	// 			.send(ZIP_FILE_DATA)
-	// 			.set("Content-Type", "application/x-zip-compressed")
-	// 			.then(function (res: Response) {
-	// 				// some logging here please!
-	// 				expect(res.status).to.be.equal(200);
-	// 				console.log("PUT request successful");
-	// 			})
-	// 			.catch(function (err) {
-	// 				// some logging here please!
-	// 				console.error("PUT request failed", err.message);
-	// 				expect.fail();
-	// 			});
-	// 	} catch (err) {
-	// 		// and some more logging here!
-	// 	}
-	// });
-	//
-	// it("PUT test for rooms dataset", function () {
-	// 	const ENDPOINT_URL = "/dataset/rooms/rooms";
-	// 	const ZIP_FILE_DATA = "test/resources/archives/pair.zip";
-	// 	try {
-	// 		return request(SERVER_URL)
-	// 			.put(ENDPOINT_URL)
-	// 			.send(ZIP_FILE_DATA)
-	// 			.set("Content-Type", "application/x-zip-compressed")
-	// 			.then(function (res: Response) {
-	// 				// some logging here please!
-	// 				expect(res.status).to.be.equal(200);
-	// 				console.log("PUT request successful");
-	// 			})
-	// 			.catch(function (err) {
-	// 				// some logging here please!
-	// 				console.error("PUT request failed", err.message);
-	// 				expect.fail();
-	// 			});
-	// 	} catch (err) {
-	// 		// and some more logging here!
-	// 	}
-	// });
-	//
-	// // The other endpoints work similarly. You should be able to find all instructions at the supertest documentation
-	// // DELETE test for courses dataset
-	// it("DELETE test for courses dataset", function () {
-	// 	const ENDPOINT_URL = "/dataset/sections/sections";
-	// 	return request(SERVER_URL)
-	// 		.delete(ENDPOINT_URL)
-	// 		.then(function (res: Response) {
-	// 			// Logging success
-	// 			expect(res.status).to.be.equal(200);
-	// 			console.log("DELETE request successful");
-	// 		})
-	// 		.catch(function (err) {
-	// 			// Logging error
-	// 			console.error("DELETE request failed", err.message);
-	// 			expect.fail();
-	// 		});
-	// });
-	//
-	// // POST test for query on dataset
-	// it("POST test for query on dataset to return properly", function () {
-	// 	const ENDPOINT_URL = "/query"; // adjust as necessary
-	// 	const QUERY_DATA = {
-	// 		WHERE: {
-	// 			GT: {
-	// 				sections_avg: 97
-	// 			}
-	// 		},
-	// 		OPTIONS: {
-	// 			COLUMNS: [
-	// 				"sections_dept",
-	// 				"sections_avg"
-	// 			],
-	// 			ORDER: "sections_avg"
-	// 		}
-	// 	};
-	// 	return request(SERVER_URL)
-	// 		.post(ENDPOINT_URL)
-	// 		.send(QUERY_DATA)
-	// 		.then(function (res: Response) {
-	// 			// Logging success
-	// 			expect(res.status).to.be.equal(200);
-	// 			console.log("POST request successful");
-	// 		})
-	// 		.catch(function (err) {
-	// 			// Logging error
-	// 			console.error("POST request failed", err.message);
-	// 			expect.fail();
-	// 		});
-	// });
+	it("PUT test for sections dataset", function () {
+		const ENDPOINT_URL = "/dataset/ubc/sections";
+		const ZIP_FILE_DATA = fs.readFileSync("test/resources/archives/pair.zip").toString("base64");
+		try {
+			return request(SERVER_URL)
+				.put(ENDPOINT_URL)
+				.send(ZIP_FILE_DATA)
+				.set("Content-Type", "text/plain")
+				.then(function (res: Response) {
+					// some logging here please!
+					expect(res.status).to.be.equal(200);
+					console.log("PUT request successful");
+				})
+				.catch(function (err) {
+					// some logging here please!
+					console.error("PUT request failed", err.message);
+					expect.fail();
+				});
+		} catch (err) {
+			// and some more logging here!
+		}
+	});
+
+	it("PUT test for rooms dataset", function () {
+		const ENDPOINT_URL = "/dataset/ubcc/rooms";
+		const ZIP_FILE_DATA = fs.readFileSync("test/resources/archives/campus.zip").toString("base64");
+		try {
+			return request(SERVER_URL)
+				.put(ENDPOINT_URL)
+				.send(ZIP_FILE_DATA)
+				.set("Content-Type", "text/plain") // application/x-zip-compressed
+				.then(function (res: Response) {
+					// some logging here please!
+					expect(res.status).to.be.equal(200);
+					console.log("PUT request successful");
+				})
+				.catch(function (err) {
+					// some logging here please!
+					console.error("PUT request failed", err.message);
+					expect.fail();
+				});
+		} catch (err) {
+			// and some more logging here!
+		}
+	});
+
+	// DELETE test for courses dataset
+	it("DELETE test 404", function () {
+		const ENDPOINT_URL = "/dataset/ubc";
+		return request(SERVER_URL)
+			.delete(ENDPOINT_URL)
+			.then(function (res: Response) {
+				// Logging success
+				expect(res.status).to.be.equal(404);
+				console.log("DELETE not found error");
+			})
+			.catch(function (err) {
+				expect.fail();
+			});
+	});
+
+	it("DELETE test for courses dataset", async function () {
+		await facade.addDataset("ubc", sections, InsightDatasetKind.Sections);
+		const ENDPOINT_URL = "/dataset/ubc";
+		return request(SERVER_URL)
+			.delete(ENDPOINT_URL)
+			.then(function (res: Response) {
+				// Logging success
+				expect(res.status).to.be.equal(200);
+				console.log("DELETE request successful");
+			})
+			.catch(function (err) {
+				// Logging error
+				console.error("DELETE request failed", err.message);
+				expect.fail();
+			});
+	});
+
+	// POST test for query on dataset
+	it("POST test for query on dataset to return properly", async function () {
+		await clearDisk();
+		await facade.addDataset("sections", sections, InsightDatasetKind.Sections);
+		const ENDPOINT_URL = "/query";
+		const QUERY_DATA = {
+			WHERE: {
+				GT: {
+					sections_avg: 97
+				}
+			},
+			OPTIONS: {
+				COLUMNS: [
+					"sections_dept",
+					"sections_avg"
+				],
+				ORDER: "sections_avg"
+			}
+		};
+		return request(SERVER_URL)
+			.post(ENDPOINT_URL)
+			.send(QUERY_DATA)
+			.then(function (res: Response) {
+				// Logging success
+				expect(res.status).to.be.equal(200);
+				console.log("POST request successful");
+			})
+			.catch(function (err) {
+				// Logging error
+				console.error("POST request failed", err.message);
+				expect.fail();
+			});
+	});
 
 	it("POST test for query on dataset to fail", function () {
 		const ENDPOINT_URL = "/query"; // adjust as necessary
